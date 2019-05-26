d78d5d07504dbc38d42203c336884959a624d072
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2019-05-26 16:42:20 -0300

    Inclusão de variaveis na entidade usuario

--- src/main/java/com/felipegabriel/merxfinanceiro/model/Usuario.java
+++ src/main/java/com/felipegabriel/merxfinanceiro/model/Usuario.java
@@ -1,5 +1,7 @@
 package com.felipegabriel.merxfinanceiro.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -45,27 +47,49 @@ public class Usuario {
 	@Setter
 	private String usuarioPerfil;
 	
+	@Column(nullable = false)
+	@Getter
+	@Setter
+	private String nome;	
+	
+	@Column(name = "datanascimento", nullable = false)
+	@Getter
+	@Setter
+	private Date dataNascimento;
+	
+	@Column(nullable = false)
+	@Getter
+	@Setter
+	private String sexo;
+	
 	public Usuario() {
 		
 	}
 
-	public Usuario(Integer pkUsuario, String email, String senha, String token, String foto, String usuarioPerfil) {
+	public Usuario(Integer pkUsuario, String email, String senha, String token, String foto, String usuarioPerfil,
+			String nome, Date dataNascimento, String sexo) {
 		this.pkUsuario = pkUsuario;
 		this.email = email;
 		this.senha = senha;
 		this.token = token;
 		this.foto = foto;
 		this.usuarioPerfil = usuarioPerfil;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.sexo = sexo;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((foto == null) ? 0 : foto.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		result = prime * result + ((pkUsuario == null) ? 0 : pkUsuario.hashCode());
 		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
 		result = prime * result + ((token == null) ? 0 : token.hashCode());
 		result = prime * result + ((usuarioPerfil == null) ? 0 : usuarioPerfil.hashCode());
 		return result;
@@ -80,6 +104,11 @@ public class Usuario {
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario other = (Usuario) obj;
+		if (dataNascimento == null) {
+			if (other.dataNascimento != null)
+				return false;
+		} else if (!dataNascimento.equals(other.dataNascimento))
+			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
@@ -90,6 +119,11 @@ public class Usuario {
 				return false;
 		} else if (!foto.equals(other.foto))
 			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
 		if (pkUsuario == null) {
 			if (other.pkUsuario != null)
 				return false;
@@ -99,6 +133,11 @@ public class Usuario {
 			if (other.senha != null)
 				return false;
 		} else if (!senha.equals(other.senha))
+			return false;
+		if (sexo == null) {
+			if (other.sexo != null)
+				return false;
+		} else if (!sexo.equals(other.sexo))
 			return false;
 		if (token == null) {
 			if (other.token != null)
@@ -112,6 +151,8 @@ public class Usuario {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
