7bd6e79eb7fb3e2872570ade3db533debe3b70fa
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2019-06-16 22:37:26 -0300

    Criação de metodo de listagem de usuarios por paginação

--- src/main/java/com/felipegabriel/merxfinanceiro/resource/UsuarioResource.java
+++ src/main/java/com/felipegabriel/merxfinanceiro/resource/UsuarioResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipegabriel.merxfinanceiro.model.Usuario;
@@ -24,12 +26,12 @@ import com.felipegabriel.merxfinanceiro.service.UsuarioService;
 @RequestMapping(value = "/usuarios")
 @CrossOrigin
 public class UsuarioResource {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@PostMapping
-	public ResponseEntity<Usuario> salvar(@Valid @RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> salvar(@Valid @RequestBody Usuario usuario) {
 		try {
 			Usuario obj = usuarioService.salvarUsuario(usuario);
 			return ResponseEntity.ok(obj);
@@ -37,19 +39,19 @@ public class UsuarioResource {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Usuario> atualizar(@Valid @RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> atualizar(@Valid @RequestBody Usuario usuario) {
 		try {
-			Usuario obj = usuarioService.salvarUsuario(usuario);
+			Usuario obj = usuarioService.editarUsuario(usuario);
 			return ResponseEntity.ok(obj);
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 	}
-	
+
 	@DeleteMapping(value = "/{pkUsuario}")
-	public ResponseEntity<Void> deletar(@Valid @PathVariable Integer pkUsuario){
+	public ResponseEntity<Void> deletar(@Valid @PathVariable Integer pkUsuario) {
 		try {
 			usuarioService.deletarUsuario(pkUsuario);
 			return ResponseEntity.noContent().build();
@@ -57,9 +59,9 @@ public class UsuarioResource {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 	}
-	
+
 	@GetMapping(value = "/{pkUsuario}")
-	public ResponseEntity<Usuario> buscar(@Valid @PathVariable Integer pkUsuario){
+	public ResponseEntity<Usuario> buscar(@Valid @PathVariable Integer pkUsuario) {
 		try {
 			Usuario obj = usuarioService.buscarUsuario(pkUsuario);
 			return ResponseEntity.ok(obj);
@@ -67,9 +69,9 @@ public class UsuarioResource {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 	}
-	
+
 	@GetMapping(value = "/listar/")
-	public ResponseEntity<List<Usuario>> listar(){
+	public ResponseEntity<List<Usuario>> listar() {
 		try {
 			List<Usuario> listObj = usuarioService.listarUsuarios();
 			return ResponseEntity.ok(listObj);
@@ -77,12 +79,33 @@ public class UsuarioResource {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 	}
-	
+
+	@GetMapping(value = "/listar-usuario-paginacao")
+	public ResponseEntity<Page<Usuario>> listarUsuarioPaginacao(@RequestParam(value = "pagina") Integer pagina,
+			@RequestParam(value = "tamanho") Integer tamanho) {
+		try {
+			Page<Usuario> listObj = usuarioService.listarUsuariosPorPaginacao(pagina, tamanho);
+			return ResponseEntity.ok(listObj);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
+
 	@GetMapping(value = "/email/{email}")
-	public ResponseEntity<Usuario> listar(@Valid @PathVariable String email){
+	public ResponseEntity<Usuario> listar(@Valid @PathVariable String email) {
 		try {
 			Usuario obj = usuarioService.buscarUsuarioPorEmail(email);
 			return ResponseEntity.ok(obj);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
+
+	@GetMapping(value = "/nome/{nome}")
+	public ResponseEntity<List<Usuario>> buscarUsuarioPorNome(@Valid @PathVariable String nome) {
+		try {
+			List<Usuario> listObj = usuarioService.buscarUsuarioPorNome(nome);
+			return ResponseEntity.ok(listObj);
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
--- src/main/java/com/felipegabriel/merxfinanceiro/service/UsuarioService.java
+++ src/main/java/com/felipegabriel/merxfinanceiro/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package com.felipegabriel.merxfinanceiro.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,32 +14,51 @@ import com.felipegabriel.merxfinanceiro.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
-	
+
 	public Usuario salvarUsuario(Usuario usuario) {
 		usuario.setSenha(pe.encode(usuario.getSenha()));
 		return usuarioRepository.save(usuario);
 	}
-	
+
+	public Usuario editarUsuario(Usuario usuario) {
+		if (usuario.getSenha() != null) {
+			usuario.setSenha(pe.encode(usuario.getSenha()));
+		} else {
+			usuario.setSenha(buscarUsuario(usuario.getPkUsuario()).getSenha());
+		}
+		return usuarioRepository.save(usuario);
+	}
+
 	public void deletarUsuario(Integer pkUsuario) {
 		usuarioRepository.deleteById(pkUsuario);
 	}
-	
+
 	public Usuario buscarUsuario(Integer pkUsuario) {
 		return usuarioRepository.findById(pkUsuario).orElse(null);
+	}
+
+	public Usuario buscarUsuarioPorEmail(String email) {
+		return usuarioRepository.findByEmail(email);
+	}
+
+	public List<Usuario> buscarUsuarioPorNome(String nome) {
+		return usuarioRepository.findAll().stream().filter(x -> x.getNome().toUpperCase().contains(nome.toUpperCase()))
+				.collect(Collectors.toList());
+	}
+	
+	public Page<Usuario> listarUsuariosPorPaginacao(Integer pagina, Integer tamanho) {
+		PageRequest pageRequest = PageRequest.of(pagina, tamanho);
+		return usuarioRepository.findAll(pageRequest);
 	}
 	
 	public List<Usuario> listarUsuarios() {
 		return usuarioRepository.findAll();
 	}
-	
-	public Usuario buscarUsuarioPorEmail(String email) {
-		return usuarioRepository.findByEmail(email);
-	}
-	
+
 }
